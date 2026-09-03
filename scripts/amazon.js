3cebe3e30fd8f38a59622c51a8b125e09df8fbc9
mclejos <alejosmarkchristian@gmail.com>
2026-09-03 23:36:46 +0800

    Fix add-to-cart button data attribute to use product ID instead of name

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach((product) => {
       </div>
 
       <button class="add-to-cart-button button-primary
-      js-add-to-cart" data-product-name="${product.name}">
+      js-add-to-cart" data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
@@ -58,12 +58,12 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button, index) => {
   button.addEventListener('click', () => {
-    const productName = button.dataset.productName;
+    const productId = button.dataset.productId;
     
     let matchingItem;
 
     cart.forEach((item) => {
-      if (productName === item.productName) {
+      if (productId === item.productId) {
         matchingItem = item;
 
       }
@@ -73,7 +73,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button, index) => {
       matchingItem.quantity += 1;
     }else{
       cart.push({
-        productName: productName,
+        productId: productId,
         quantity: 1
       });
     }           
