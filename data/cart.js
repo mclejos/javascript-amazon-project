c335091853d0ae5c16db9df45c85f5db0b27f1b9
mclejos <alejosmarkchristian@gmail.com>
2026-09-08 23:26:24 +0800

    Refactor cart and product handling to use ES6 modules and improve cart quantity updates

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
-const cart = [];
+export const cart = [];
+
+export function addToCart(productId, quantity) {
+    let matchingItem;
+
+    cart.forEach((cartitem) => {
+      if (productId === cartitem.productId) {
+        matchingItem = cartitem;
+
+      }
+    });
+    
+    if (matchingItem) {
+      matchingItem.quantity += quantity;
+    }else{
+      cart.push({
+        productId,
+        quantity
+      });
+    }
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,6 @@
+import { cart, addToCart } from '../data/cart.js';
+import { products } from '../data/products.js';
+
 let productsHTML = '';
 const addedMessageTimeouts = {};
 
@@ -57,39 +60,26 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const { productId } = button.dataset;
     const quantity = Number(
       document.querySelector(`.js-quantity-selector-${productId}`).value
     );
-    
-    let matchingItem;
+    addToCart(productId, quantity);
 
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-
-      }
-    });
-    
-    if (matchingItem) {
-      matchingItem.quantity += quantity;
-    }else{
-      cart.push({
-        productId,
-        quantity
-      });
-    }
-
-    let cartQuantity = 0;
-
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-
-    document.querySelector('.js-cart-quantity')
-      .innerHTML = cartQuantity;
+    updateCartQuantity();
 
     const addedMessage = document.querySelector(
       `.js-added-to-cart-${productId}`
