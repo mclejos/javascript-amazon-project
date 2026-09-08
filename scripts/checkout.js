1d0d6b50e7dd2bc266c5894cac564230d8d59f88
mclejos <alejosmarkchristian@gmail.com>
2026-09-09 00:33:41 +0800

    Integrate removeFromCart function into checkout process and update delete link functionality

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js';
+import { cart, removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -39,7 +39,7 @@ cart.forEach((cartItem) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link data-product-id="${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -52,7 +52,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio" checked
             class="delivery-option-input"
-            name="$delivery-option-${matchingProduct.id}">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Tuesday, June 21
@@ -94,7 +94,14 @@ cart.forEach((cartItem) => {
   `;
 });
 
-console.log(cartSummaryHTML);
-
 document.querySelector('.js-order-summary')
   .innerHTML = cartSummaryHTML;
+
+document.querySelector('.js-delete-link')
+  .forEach((link)=>{
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+    });
+  });
+  
